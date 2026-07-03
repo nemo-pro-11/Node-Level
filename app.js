52ef104aefbc0990093fdf6757abe26acfb12f1a
Nemo <Nemo@gmail.com>
2026-07-03 14:31:10 +0300

    Start project

--- app.js
+++ app.js
@@ -1,94 +1,65 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 
+// Wab Page preparation 
 
-
-
-// app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-  // res.sendFile( __dirname + "/Views/index.html")
-  // res.sendFile( "/Views/index.html" , {root : __dirname} )
-
-  Article.find().then((data) => {
-    res.render("home.ejs" , { Hi : "Home padg" ,  arr : data })
-  }).catch( (err) => {
-    console.log(err)
-  })
-
-
-})
-
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://Nemo1:1QAZ@cluster0.qg5noil.mongodb.net/AllData?appName=Cluster0")
-.then(() => {
-  app.listen(port, () => {
-    console.log(`http://localhost:${port}`);
-  });
- })
- .catch((err) => {
-   console.log(err);
- });
-
-// mongoose.connect("mongodb://localhost:27017")
-app.use(express.urlencoded({ extended: true }));
-const Article = require("./models/mySchema")
-
-
-// Using Model.create()
-
-app.post("/", (req, res) => {
-  Article
-    .create(req.body)
-    .then( () => {
-      res.redirect("/hi");
-      console.log(req.body)
-
-    })
-    .catch( err => {
-      console.log(err);
-    });
+app.set("view engine" , "ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs", {});
 });
- 
-app.get('/hi', (req, res) => {
-  res.send("<h1>تمت العمليه ب نجاح</h1>")
-})
 
-// Using Model.save()
+app.get("/user/add.html", (req, res) => {
+  res.render("user/add.ejs", {});
+});
 
-// app.post("/", (req, res) => {
-//   const article1 = new Article(req.body);
-//   article1
-//     .save()
-//     .then(result => {
-//       res.redirect("/hi");
-//       console.log(req.body)
-//     })
-//     .catch( err => {
-//       console.log(err);
-//     });
-// });
+app.get("/user/view.html", (req, res) => {
+  res.render("user/view.ejs", {});
+});
+
+app.get("/user/edit.html", (req, res) => {
+  res.render("user/edit.ejs", {});
+});
+
+app.get("/user/search.html", (req, res) => {
+  res.render("user/search.ejs", {});
+});
 
 
 
+// connected Database
+const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Article = require("./models/mySchema");
+
+mongoose
+  .connect(
+    "mongodb+srv://Nemo1:1QAZ@cluster0.qg5noil.mongodb.net/AllData?appName=Cluster0",
+  // mongoose.connect("mongodb://localhost:27017")
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 
-
-app.use(express.static('public'))
-
+// connected Style
+app.use(express.static("public"));
 //auto Style
- 
+
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname, 'public'));
- 
- 
+liveReloadServer.watch(path.join(__dirname, "public"));
+
 const connectLivereload = require("connect-livereload");
 app.use(connectLivereload());
- 
+
 liveReloadServer.server.once("connection", () => {
   setTimeout(() => {
     liveReloadServer.refresh("/");
